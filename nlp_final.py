79b3a5439f875249e0fb97d7efc42ec8cfe4468f
jallurikoushik098 <71833261+jallurikoushik098@users.noreply.github.com>
2021-10-21 15:41:56 +0530

    Update nlp_final.py

--- nlp_final.py
+++ nlp_final.py
@@ -9,15 +9,6 @@ ner=spacy.load("en_core_web_sm")
 def scrapping(input1):
   result = wikipedia.search(input1)
   print(result)
-  if(result[1] == true):
-        page = wikipedia.page(result[1])
-	content = page.content
-        print(content)
-        return content
-   else:
-	st.write("sorry no info available")
-		
-	
   content = page.content
   print(content)
   return content
